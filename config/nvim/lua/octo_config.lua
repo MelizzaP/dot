966e49a4ffbfda92e6dac85163ec04297d88d93c
Melissa Patterson <melissa.patterson.va@gmail.com>
2026-05-19 10:15:55 -0400

    Misc

--- config/nvim/lua/octo_config.lua
+++ config/nvim/lua/octo_config.lua
@@ -212,7 +212,7 @@ require"octo".setup({
     },
     submit_win = {
       approve_review = { lhs = "<C-a>", desc = "approve review", mode = { "n", "i" } },
-      comment_review = { lhs = "<C-m>", desc = "comment review", mode = { "n", "i" } },
+      comment_review = { lhs = "<C-s>", desc = "comment review", mode = { "n", "i" } },
       request_changes = { lhs = "<C-r>", desc = "request changes review", mode = { "n", "i" } },
       close_review_tab = { lhs = "<C-c>", desc = "close review tab", mode = { "n" } },
     },
